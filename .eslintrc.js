c42b4e8a205afc70fa2534503650cb1b4480b3ba
oThinas <thiagomartinsprado@gmail.com>
2022-12-14 00:54:42 -0300

    feat: rotas de get all e de post implementada feat: validação dos campos implementada

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,118 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir : __dirname, 
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/recommended",
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/interface-name-prefix": "off",
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+
+    // General
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+
+    // Variables
+    "prefer-const": "error",
+    "no-const-assign": "error",
+    "no-var": "error",
+    "no-undef": "error",
+    "one-var": ["error", "never"],
+    "no-multi-assign": "error",
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+
+    // Object
+    "no-new-object": "error",
+    "object-shorthand": "error",
+    "quote-props": ["error", "as-needed"],
+    "no-prototype-builtins": "error",
+    
+    // Properties
+    "dot-notation": "error",
+    
+    // Array
+    "no-array-constructor": "error",
+    "array-callback-return": ["error", { "allowImplicit": true, "checkForEach": true }],
+    
+    // Strings
+    "prefer-template": "error",
+    "template-curly-spacing": ["error", "never"],
+    "no-eval": "error",
+    "no-useless-escape": "error",
+    
+    // Default Functions
+    "default-param-last": "error",
+    "no-new-func": "error",
+    "space-before-blocks": ["error", "always"],
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "never",
+      "asyncArrow": "always"
+    }],
+    "no-param-reassign": "error",
+    "prefer-spread": "error",
+    "function-paren-newline": "error",
+    
+    // Arrow Functions
+    "prefer-arrow-callback": "error",
+    "arrow-spacing": "error",
+    "arrow-parens": "error",
+    "no-confusing-arrow": ["error", { "allowParens": false }],
+    "implicit-arrow-linebreak": "error",
+    "no-duplicate-imports": "error",
+    "object-curly-newline": ["error", { "multiline": true }],
+    
+    // Operators
+    "eqeqeq": "error",
+    "no-case-declarations": "error",
+    "no-unneeded-ternary": "error",
+    "no-mixed-operators": "error",
+    "space-infix-ops": "error",
+    
+    // Blocks
+    "nonblock-statement-body-position": "error",
+    "brace-style": "error",
+    "no-else-return": "error",
+
+    // Comments
+    "spaced-comment": "error",
+    
+    // Whitespace
+    "indent": ["error", 2],
+    "eol-last": "error",
+    "newline-per-chained-call": "error",
+    "no-whitespace-before-property": "error",
+    "padded-blocks": ["error", "never"],
+    "no-multiple-empty-lines": "error",
+    "space-in-parens": "error",
+    "array-bracket-spacing": "error",
+    "object-curly-spacing": ["error", "always"],
+    "comma-spacing": "error",
+    "computed-property-spacing": "error",
+    "func-call-spacing": "error",
+    "key-spacing": "error",
+    "no-trailing-spaces": "error",
+    "comma-style": "error",
+    "comma-dangle": ["error", "always-multiline"],
+
+    // Types
+    "no-new-wrappers": "error",
+
+    // Naming
+    "camelcase": "error",
+    "no-underscore-dangle": ["error", { "allow": ["_count"] }]
   },
 };
